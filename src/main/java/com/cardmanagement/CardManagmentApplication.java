1c007a72cd1662381a44fac469691e9a992ddd7b
Mr Smith <brandoncontu45@gmail.com>
2024-01-25 09:34:51 -0500

    first commit
    first commit

---
+++ src/main/java/com/cardmanagement/CardManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.cardmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardManagmentApplication.class, args); 
+	}
+
+}
---
+++ src/test/java/com/cardmanagement/CardManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
